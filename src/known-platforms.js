eb52791d6c0aadb76d3f69e50beab46e7f98df2b
Marek Sierociński <marek.sierocinski@bitbar.com>
2020-09-09 20:38:39 +0200

    Fix for ChromeOS

--- src/known-platforms.js
+++ src/known-platforms.js
@@ -6,7 +6,7 @@ const platforms = [
   new Entity('MacOS', /OS X ([0-9._]+)/),
   new Entity('iOS', /iPhone OS ([0-9_.]+)/),
   new Entity('iPadOS', /iPad.+?OS ([0-9_,]+)/),
-  new Entity('CrOS', /CrOS [^ ]+ ([0-9.]+)/),
+  new Entity('ChromeOS', /CrOS [^ ]+ ([0-9.]+)/),
   new Entity('Android', /(?:Android|Adr) ([0-9.]+)/),
   new Entity('BlackBerry', /BlackBerry|BB10/),
   new Entity('webOS', /webOS\/([0-9.]+)/),
