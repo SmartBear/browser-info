d4e1908fb30c0a66e30398072667ff0358a90e4a
Marek Sierociński <marek.sierocinski@bitbar.com>
2020-10-26 21:43:37 +0100

    Added SamsungInternet browser

--- src/known-browsers.js
+++ src/known-browsers.js
@@ -2,8 +2,9 @@ import { Entity } from './Entity';
 
 const browsers = [
   new Entity('Firefox', /Firefox\/([0-9.]+)/),
-  new Entity('Opera', /OPR\/([0-9.]+)/),
   new Entity('Edge', /Edge\/([0-9.]+)/),
+  new Entity('Opera', /OPR\/([0-9.]+)/),
+  new Entity('SamsungInternet', /SamsungBrowser\/([0-9.]+)/),
   new Entity('Maxthon', /Maxthon\/([0-9.]+)/),
   new Entity('Chrome', /Chrome\/([0-9.]+)/),
   new Entity('Safari', /Version\/([0-9.]+) Safari\//),
