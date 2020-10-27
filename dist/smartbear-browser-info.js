abc641b0a122d59f950e23d164c827bc1c25b1d9
Marek Sierociński <marek.sierocinski@bitbar.com>
2020-10-27 08:21:58 +0100

    version: 1.2.2

--- dist/smartbear-browser-info.js
+++ dist/smartbear-browser-info.js
@@ -1,4 +1,4 @@
-/* @smartbear/browser-info v1.2.1 | Copyright 2020 (c) SmartBear Software and contributors | https://github.com/SmartBear/browser-info/blob/master/LICENSE */
+/* @smartbear/browser-info v1.2.2 | Copyright 2020 (c) SmartBear Software and contributors | https://github.com/SmartBear/browser-info/blob/master/LICENSE */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
